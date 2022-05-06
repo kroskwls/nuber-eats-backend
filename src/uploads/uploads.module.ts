1dcf86acf166f6cf39535a3ebec527f2873ea21a
kroskwls <kroskwls@gmail.com>
2022-05-06 15:41:24 +0900

    ConfigService

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UploadsController } from './uploads.controller';
 
 @Module({
-	controllers: [UploadsController]
+	controllers: [UploadsController],
+	providers: [ConfigService]
 })
 export class UploadsModule { }
