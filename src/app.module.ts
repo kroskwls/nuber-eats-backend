df34278570bd5d7ddb763516b6def6a6f5f4b5d2
kroskwls <kroskwls@gmail.com>
2022-05-06 16:12:41 +0900

    database connect url

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,11 +35,11 @@ const TOKEN_KEY = 'x-jwt';
 			ignoreEnvFile: process.env.NODE_ENV === 'production',
 			validationSchema: Joi.object({
 				NODE_ENV: Joi.string().valid('dev', 'production', 'test').required(),
-				DB_HOST: Joi.string().required(),
-				DB_PORT: Joi.string().required(),
-				DB_USERNAME: Joi.string().required(),
-				DB_PASSWORD: Joi.string().required(),
-				DB_NAME: Joi.string().required(),
+				DB_HOST: Joi.string(),
+				DB_PORT: Joi.string(),
+				DB_USERNAME: Joi.string(),
+				DB_PASSWORD: Joi.string(),
+				DB_NAME: Joi.string(),
 				PRIVATE_KEY: Joi.string().required(),
 				MAILGUN_API_KEY: Joi.string().required(),
 				MAILGUN_DOMAIN_NAME: Joi.string().required(),
@@ -48,11 +48,17 @@ const TOKEN_KEY = 'x-jwt';
 		}),
 		TypeOrmModule.forRoot({
 			type: 'postgres',
-			host: process.env.DB_HOST,
-			port: +process.env.DB_PORT,
-			username: process.env.DB_USERNAME,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
+			...(
+				process.env.DATABASE_URL ? ({
+					url: process.env.DATABASE_URL,
+				}) : ({
+					host: process.env.DB_HOST,
+					port: +process.env.DB_PORT,
+					username: process.env.DB_USERNAME,
+					password: process.env.DB_PASSWORD,
+					database: process.env.DB_NAME,
+				})
+			),
 			synchronize: process.env.NODE_ENV !== 'production',
 			// logging: process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
 			logging: false,
